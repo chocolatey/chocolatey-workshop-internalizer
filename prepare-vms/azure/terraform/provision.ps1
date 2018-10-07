033b8d09fb439417ac95912228b6512447c7fe8e
Gary Ewan Park <gep13@gep13.co.uk>
2018-10-07 15:50:53 -0700

    (build) Moving windows setup in terraform provision

--- prepare-vms/azure/terraform/provision.ps1
+++ prepare-vms/azure/terraform/provision.ps1
@@ -28,8 +28,37 @@ refreshenv
 $env:PATH=$env:PATH + ';C:\Program Files\Mozilla Firefox;C:\Program Files\Microsoft VS Code;C:\Program Files\Git\bin'
 [Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::Machine)
 
-# Atom is installed to C:\Users\training\AppData\Local\atom so we have to do it in Terraform and not Packer
-choco install -y atom
+# Adapted from http://stackoverflow.com/a/29571064/18475
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+New-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force | Out-Null
+New-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force | Out-Null
+#Stop-Process -Name Explorer -Force
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled."
+
+# http://techrena.net/disable-ie-set-up-first-run-welcome-screen/
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1 -PropertyType "DWord" -Force | Out-Null
+Write-Host "IE first run welcome screen has been disabled."
+
+Write-Host "Set Windows Updates to manual"
+Cscript $env:WinDir\System32\SCregEdit.wsf /AU 1
+Net stop wuauserv
+Net start wuauserv
+
+# Ensure there is a profile file so we can get tab completion
+New-Item -ItemType Directory $(Split-Path $profile -Parent) -Force
+Set-Content -Path $profile -Encoding UTF8 -Value "" -Force
+
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
+
+Write-Host "Disable Windows Defender"
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+Write-Host "Do not open Server Manager at logon"
+New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "1" -Force
+
+Write-Host "Disable autologon"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -PropertyType DWORD -Value "0" -Force
 
 # Create shortcuts
 $WshShell = New-Object -comObject WScript.Shell
