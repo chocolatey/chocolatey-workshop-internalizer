0c39e6ac52d48ad0d2b5b3b9bc7cdfad49e00dd8
Paul Broadwith <paul@pauby.com>
2018-11-12 16:10:43 +0000

    (maint) Add Pester

--- prepare-vms/scripts/InstallAdditionalApps.ps1
+++ prepare-vms/scripts/InstallAdditionalApps.ps1
@@ -2,7 +2,7 @@ if ($null -eq (Get-Command -Name 'choco.exe' -ErrorAction SilentlyContinue)) {
   Write-Warning "Chocolatey not installed. Cannot install standard packages."
 }
 else {
-  @( 'vscode', 'git', 'googlechrome', 'firefox' ) | ForEach-Object {
+  @( 'vscode', 'git', 'googlechrome', 'firefox', 'pester' ) | ForEach-Object {
       choco upgrade $_ -y --no-progress
   }
 }
