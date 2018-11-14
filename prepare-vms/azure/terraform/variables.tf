4f01962a778048cd494237ec37484ca932b5dadc
Gary Ewan Park <gep13@gep13.co.uk>
2018-11-14 17:53:01 +0000

    Updated build image name [ci skip]

--- prepare-vms/azure/terraform/variables.tf
+++ prepare-vms/azure/terraform/variables.tf
@@ -30,7 +30,7 @@ variable "admin_username" {
 }
 
 variable "workshop_image" {
-  default = "windows_2016_571"}
+  default = "windows_2016_586"}
 
 variable "vm_size" {
   default = "Standard_D4s_v3"
